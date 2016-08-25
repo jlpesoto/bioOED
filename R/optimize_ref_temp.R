63b07fdbc22f31ace181d45193e315a2c871e246
Alberto <garre.alberto@gmail.com>
2016-08-25 10:18:47 +0200

    D-value is now updated in refTemp optimization
    Changing the reference temperature (obviously) changes the D-value at the reference temperature. It has been implemented for the optimization.

--- R/optimize_ref_temp.R
+++ R/optimize_ref_temp.R
@@ -9,7 +9,8 @@
 #' target for the optimization is the maximization of the determinant
 #' of the correlation matrix between parameter sensitivities. The
 #' Brent method is used, as it is the recommended one for unidimensional
-#' optimization.
+#' optimization.  
+#' The parameters z and D/delta cannot be fixed.
 #' 
 #' @param temp_ref0 Initial value of the reference temperature to use for
 #' the optimization.
@@ -44,6 +45,7 @@ optimize_refTemp <- function(temp_ref0, lower, upper,
           lower = lower, upper = upper,
           inactivation_model = inactivation_model,
           parms = parms, temp_profile = temp_profile,
+          temp_ref0 = temp_ref0,
           n_times = n_times,
           parms_fix = parms_fix, method = "Brent",
           control = list(fnscale = -1))
@@ -55,10 +57,36 @@ optimize_refTemp <- function(temp_ref0, lower, upper,
 #' Hanlder for the Optimization of Reference Temperature
 #' 
 refTemp_optim_handler <- function(temp_ref, inactivation_model, parms,
-                                  temp_profile, parms_fix, n_times) {
+                                  temp_profile, parms_fix, n_times,
+                                  temp_ref0) {
     
     
     parms_fix <- c(temp_ref = temp_ref, parms_fix)
+    
+    ## Translate the D/delta to the new reference temperature
+    
+    parms <- as.list(parms)
+    
+    if (grepl(inactivation_model, "Bigelow")) {
+        
+        new_D <- 10^(log10(parms$D_R) - (temp_ref - temp_ref0)/parms$z)
+        parms$D_R <- new_D
+        
+    } else if (grepl(inactivation_model, "Mafart")) {
+        
+        new_delta <- 10^(log10(parms$delta_ref) - (temp_ref - temp_ref0)/parms$z)
+        parms$delta_ref <- new_delta
+        
+    } else if (grepl(inactivation_model, "Geeraerd")) {
+        
+        new_D <- 10^(log10(parms$D_R) - (temp_ref - temp_ref0)/parms$z)
+        parms$D_R <- new_D
+        
+    }
+    
+    parms <- unlist(parms)
+    
+    ## Calculte correlations
     
     correlations <- calculate_pars_correlation(inactivation_model, parms,
                                                temp_profile, parms_fix,
